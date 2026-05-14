afd52985b514d96698d31db6d025448a2dbbea26
Mohamed Elshaarawy <elsharawy234@gmail.com>
2026-05-15 00:31:03 +0200

    lab3: pick nearby parcel

--- index.js
+++ index.js
@@ -11,10 +11,47 @@ const beliefset = new Map();
 const start = Date.now();
 
 var OBSERVATION_DISTANCE;
+const me = {id: '', name: '', x: -1, y: -1, score: 0};
+const parcels = new Map();
 socket.onConfig( config => OBSERVATION_DISTANCE = config.GAME.player.observation_distance );
-var me;
 
-socket.onYou( m => me = m );
+socket.onYou( ( {id, name, x, y, score} ) => {
+    me.id = id;
+    me.name = name;
+    me.x = x ? x : me.x;
+    me.y = y ? y : me.y;
+    me.score = score;
+})
+
+socket.onSensing(async ( sensing ) => {
+    for ( let p of sensing.parcels ) {
+        parcels.set(`${p.x},${p.y}`, p);
+    }
+});
+
+function distance({x: x1, y: y1}, {x: x2, y: y2}) {
+    const dx = Math.abs( Math.round(x1) - Math.round(x2) )
+    const dy = Math.abs( Math.round(y1) - Math.round(y2) )
+    return dx + dy;
+}
+
+async function blindMove(target){
+    console.log(`${me.name} moving from ${me.x}, ${me.y} towards ${target.x},${target.y}`);
+
+    var m = new Promise( res => socket.onYou( m => m.x % 1 != 0 || m.y % 1 != 0 ? null : res() ) );
+
+    if ( me.x < target.x )
+        await socket.emitMove('right');
+    else if ( me.x > target.x )
+        await socket.emitMove('left');
+    
+    if ( me.y < target.y )
+        await socket.emitMove('up');
+    else if ( me.y > target.y )
+        await socket.emitMove('down');
+
+    await m;
+}
 
 socket.on('map', async (width, height, tiles) => {
     // Move along predefined path
@@ -66,3 +103,42 @@ socket.onSensing( ( sensing ) => {
     console.log(prettyPrint)
 } )
 const dist = (a1,a2) => Math.abs(a1.x-a2.x) + Math.abs(a1.y-a2.y);
+
+while (true) {
+
+    await new Promise(res => setTimeout( res, 100 ));
+
+    if ( ! me.id || ! parcels.size )
+        continue;
+
+    console.log( `me(${me.x},${me.y})`,
+        Array.from( parcels.values() )
+        .map( p => `${p.reward}@(${p.x},${p.y})` )
+        .join( ' ' )
+    );
+
+    // get nearest parcel
+    const nearest = Array.from(parcels.values())
+    .filter( p => ! p.carriedBy )
+    .sort( (a, b) => {
+        const d1 = distance( me, a );
+        const d2 = distance( me, b );
+        return d1 - d2;
+    } ).shift();
+
+    // if no parcels are available
+    if (!nearest) {
+        console.log( 'no parcels' );
+        continue;
+    }
+    
+    // else move to nearest parcel
+    console.log( 'nearest', nearest.id, nearest.x, nearest.y );
+    
+    await blindMove(nearest)
+    console.log( 'moved to parcel', nearest.id, me.x, me.y );
+    
+    await socket.emitPickup();
+    
+    console.log( 'picked up' );
+}
