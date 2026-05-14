e8c9711938dc586d717c5efa0cb2fac0e9e87297
Mohamed Elshaarawy <elsharawy234@gmail.com>
2026-05-15 00:10:48 +0200

    lab2: Sensing

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const socket = new DjsConnect(
     process.env.DELIVEROO_TOKEN
 );
 
-let myPosition = { x: 0, y: 0 };
+const beliefset = new Map();
+const start = Date.now();
 
-socket.on('you', (id, name, x, y) => {
-    myPosition = { x, y };
-});
+var OBSERVATION_DISTANCE;
+socket.onConfig( config => OBSERVATION_DISTANCE = config.GAME.player.observation_distance );
+var me;
+
+socket.onYou( m => me = m );
 
 socket.on('map', async (width, height, tiles) => {
     // Move along predefined path
@@ -28,3 +31,38 @@ socket.on('map', async (width, height, tiles) => {
     // Pickup parcels
     await socket.emitPickup();
 });
+
+socket.onSensing( ( sensing ) => {
+    const timestamp = Date.now() - start;
+    for ( let a of sensing.agents ) {
+        if ( ! beliefset.has( a.id ) )
+            beliefset.set( a.id, [] )
+        const log = {
+            id: a.id,
+            name: a.name,
+            x: a.x,
+            y: a.y,
+            score: a.score,
+            timestamp: timestamp,
+            direction: 'none'
+        }
+        const logs = beliefset.get( a.id );
+        if ( logs && logs.length>0 ) {
+            var previous = logs[logs.length-1];
+            if ( previous.x < a.x ) log.direction = 'right';
+            else if ( previous.x > a.x ) log.direction = 'left';
+            else if ( previous.y < a.y ) log.direction = 'up';
+            else if ( previous.y > a.y ) log.direction = 'down';
+            else log.direction = 'none';
+        }
+        beliefset.get( a.id )?.push( log );
+    }
+    // compute if within perceiving area
+    let prettyPrint = Array.from(beliefset.values()).map( (logs) => {
+        const {timestamp,name,x,y,direction} = logs[logs.length-1]
+        const d = dist( me, {x,y} );
+        return `${name}(${direction},${d<OBSERVATION_DISTANCE})@${timestamp}:${x},${y}`;
+    }).join(' ');
+    console.log(prettyPrint)
+} )
+const dist = (a1,a2) => Math.abs(a1.x-a2.x) + Math.abs(a1.y-a2.y);
